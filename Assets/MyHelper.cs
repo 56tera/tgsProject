41c4e0a677b4b937ff5a44a95ee1f466d30d8456
k22104kk <>
2023-06-26 15:55:45 +0900

    シーン管理導入

---
+++ Assets/MyHelper.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+static public class MyHelper
+{
+    public enum MyScene { TitleScene, MainScene };
+    public static MyScene scene;
+
+    static Dictionary<string, MyScene> sceneDic = new Dictionary<string, MyScene> {
+        {"TitleScene", MyScene.TitleScene},
+        {"MainScene", MyScene.MainScene},
+    };
+
+    //enumのシーンで取得したシーンをロード
+    public static void MyLoadScene(MyScene scene)
+    {
+
+        SceneManager.LoadScene(sceneDic.FirstOrDefault(x => x.Value == scene).Key);
+    }
+}
---
+++ Assets/TitleScene.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class TitleScene : MonoBehaviour
+{
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("ChangeScene", 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Return))
+        {
+            MyHelper.MyLoadScene(MyHelper.MyScene.MainScene);
+        }
+    }
+
+    //void ChangeScene()
+    //{
+    //    MyHelper.MyLoadScene (MyHelper.MyScene.MainScene);
+    //}
+}
