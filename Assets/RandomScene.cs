44c7bda4137db8e0ac69b6cc7b4498dac8f0112b
k23139kk <>
2023-07-13 16:37:03 +0900

    あとはスコアと描画順

---
+++ Assets/RandomScene.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RandomScene : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    public void Start()
+    {
+            
+    }
+}
